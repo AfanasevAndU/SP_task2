1aa6a65c76641bfd316db5059959084d1a0328be
Андрей Афанасьев <andrei-2005-10@mail.ru>
2024-04-15 11:00:38 +0300

    Массивы теперь рендерятся через map

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,18 @@ import { Text } from "./shared/ui/text";
 import { Image } from "./shared/ui/image";
 import { Card } from "./shared/ui/card";
 import { Container } from "./shared/ui/container";
-import avatar from "./images/avatar.jpg";
-import css from "./images/CSS.png";
-import git from "./images/git.png";
-import js from "./images/js.webp";
-import nodejs from "./images/nodejs.png";
-import react from "./images/react.jpg";
-import ts from "./images/ts.png";
-import docker from "./images/docker.png";
-import postgresql from "./images/postgresql.jpg";
-import linux from "./images/linux.jpg";
-import python from "./images/python.webp";
-import cicd from "./images/cicd.png";
-import mobx from "./images/mobx.png";
-import django from "./images/django.png";
+import { Button } from "./shared/ui/button";
+import avatar from "./assets/avatar.jpg";
+import { ShowCards } from "./shared/ui/showCards";
+import { useState } from "react";
+import { Modal } from "./shared/ui/modal";
+import { defaultKnownSkills, defaultToKnowSkills } from "./const/skills";
 
-function App() {
+export function App() {
+  const [showSkills, setShowSkills] = useState(true);
+  const [buttonText, setButtonText] = useState(true);
+  const [modal, setModal] = useState(false);
+
   return (
     <>
       <Card size="L">
@@ -26,66 +22,37 @@ function App() {
       </Card>
       <Text color="white">Компетенции:</Text>
       <Container flexDirection="row">
-        <Card size="M">
-          <Image src={css} alt="Css"></Image>
-          <Text color="black">Css</Text>
-        </Card>
-        <Card size="M">
-          <Image src={react} alt="react"></Image>
-          <Text color="black">React Native</Text>
-        </Card>
-        <Card size="M">
-          <Image src={git} alt="git"></Image>
-          <Text color="black">Git</Text>
-        </Card>
-        <Card size="M">
-          <Image src={js} alt="js"></Image>
-          <Text color="black">JS</Text>
-        </Card>
+        <Button onClick={() => {}} type="submit">
+          {`навыки > 50%`}
+        </Button>
+        <Button onClick={() => {}} type="submit">
+          {`навыки < 50%`}
+        </Button>
+
+        <Button
+          onClick={() => {
+            setShowSkills(!showSkills);
+            setButtonText(!buttonText);
+          }}
+          type="submit"
+        >
+          {showSkills ? "Скрыть компетенции" : "Показать компетенции"}
+        </Button>
+        <Button
+          onClick={() => {
+            setModal(!modal);
+          }}
+          type="submit"
+        >
+          Добавить навык
+        </Button>
       </Container>
-      <Text color="white"> Компетенции к изучению:</Text>
-      <Container flexDirection="row">
-        <Card size="M">
-          <Image src={ts} alt="typescript"></Image>
-          <Text color="black">TS</Text>
-        </Card>
-        <Card size="M">
-          <Image src={python} alt="python"></Image>
-          <Text color="black">Python</Text>
-        </Card>
-        <Card size="M">
-          <Image src={react} alt="react"></Image>
-          <Text color="black">React</Text>
-        </Card>
-        <Card size="M">
-          <Image src={nodejs} alt="nodejs"></Image>
-          <Text color="black">Node JS</Text>
-        </Card>
-        <Card size="M">
-          <Image src={docker} alt="docker"></Image>
-          <Text color="black">Docker</Text>
-        </Card>
-        <Card size="M">
-          <Image src={postgresql} alt="postgresql"></Image>
-          <Text color="black">PostgreSQL</Text>
-        </Card>
-        <Card size="M">
-          <Image src={cicd} alt="cicd"></Image>
-          <Text color="black">ci/cd</Text>
-        </Card>
-        <Card size="M">
-          <Image src={mobx} alt="mobx"></Image>
-          <Text color="black">MobX</Text>
-        </Card>
-        <Card size="M">
-          <Image src={linux} alt="linux"></Image>
-          <Text color="black">Linux</Text>
-        </Card>
-        <Card size="M">
-          <Image src={django} alt="django"></Image>
-          <Text color="black">Django</Text>
-        </Card>
-      </Container>
+      <Modal state={modal} onClick={() => {}}></Modal>
+      <ShowCards
+        show={showSkills}
+        knowSkills={defaultKnownSkills}
+        needKnowSkills={defaultToKnowSkills}
+      ></ShowCards>
     </>
   );
 }
