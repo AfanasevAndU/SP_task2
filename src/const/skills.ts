4564081deae9caad2102f7270480b14355f64d97
Андрей Афанасьев <andrei-2005-10@mail.ru>
2024-04-15 10:50:28 +0300

    Вынес массивы в константы

---
+++ src/const/skills.ts
@@ -0,0 +1,123 @@
+import css from "./images/CSS.png";
+import git from "./images/git.png";
+import js from "./images/js.webp";
+import nodejs from "./images/nodejs.png";
+import react from "./images/react.jpg";
+import ts from "./images/ts.png";
+import docker from "./images/docker.png";
+import postgresql from "./images/postgresql.jpg";
+import linux from "./images/linux.jpg";
+import python from "./images/python.webp";
+import cicd from "./images/cicd.png";
+import mobx from "./images/mobx.png";
+import django from "./images/django.png";
+
+export const defaultKnownSkills = [
+  {
+    src: git,
+    alt: "git",
+    color: "black",
+    title: "git",
+    knowledge: 100,
+    id: 0,
+  },
+  {
+    src: js,
+    alt: "js",
+    color: "black",
+    title: "js",
+    knowledge: 100,
+    id: 1,
+  },
+  {
+    src: css,
+    alt: "css",
+    color: "black",
+    title: "css",
+    knowledge: 100,
+    id: 2,
+  },
+  {
+    src: react,
+    alt: "react native",
+    color: "black",
+    title: "react native",
+    knowledge: 100,
+    id: 3,
+  },
+];
+
+export const defaultToKnowSkills = [
+  {
+    src: nodejs,
+    alt: "nodejs",
+    color: "black",
+    title: "nodejs",
+    knowledge: 65,
+    id: 0,
+  },
+  {
+    src: python,
+    alt: "python",
+    color: "black",
+    title: "python",
+    knowledge: 34,
+    id: 1,
+  },
+  {
+    src: docker,
+    alt: "docker",
+    color: "black",
+    title: "docker",
+    knowledge: 12,
+    id: 2,
+  },
+  {
+    src: postgresql,
+    alt: "postgresql",
+    color: "black",
+    title: "postgresql",
+    knowledge: 10,
+    id: 3,
+  },
+  {
+    src: mobx,
+    alt: "mobx",
+    color: "black",
+    title: "mobx",
+    knowledge: 45,
+    id: 4,
+  },
+  {
+    src: django,
+    alt: "django",
+    color: "black",
+    title: "django",
+    knowledge: 50,
+    id: 5,
+  },
+  {
+    src: cicd,
+    alt: "cicd",
+    color: "black",
+    title: "cicd",
+    knowledge: 90,
+    id: 6,
+  },
+  {
+    src: linux,
+    alt: "linux",
+    color: "black",
+    title: "linux",
+    knowledge: 82,
+    id: 7,
+  },
+  {
+    src: ts,
+    alt: "typescript",
+    color: "black",
+    title: "typescript",
+    knowledge: 75,
+    id: 8,
+  },
+];
